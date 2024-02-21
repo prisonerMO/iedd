737fbbc9c8f471b1e59023eb3965fb6d36f5bc98
prisonerMO <prisoner2519@gmail.com>
2024-02-21 12:02:48 +0200

    Added compat zen

---
+++ addons/compat_zen/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT compat_zen
+#define COMPONENT_NAME "ZEN - IEDD Compatibility"
+#include "\x\iedd\addons\main\script_mod.hpp"
+#include "\x\iedd\addons\main\script_debug.hpp"
+#ifdef DEBUG_ENABLED_COMPAT_ZEN
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_ZEN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_ZEN
+#endif
+
+#include "\x\iedd\addons\main\script_macros.hpp"
