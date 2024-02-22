442ac599c61bc31cf468b75fa264448becac9373
prisonerMO <prisoner2519@gmail.com>
2024-02-22 07:22:14 +0200

    Added init attributes to ied objects (not vest)

--- addons/compat_zen/XEH_preInit.sqf
+++ addons/compat_zen/XEH_preInit.sqf
@@ -6,4 +6,8 @@ PREP_RECOMPILE_START;
 #include "XEH_PREP.hpp"
 PREP_RECOMPILE_END;
 
+// Initialize the core/default attributes
+#include "initAttributes.sqf"
+//#include "initSettings.sqf"
+
 ADDON = true;
--- addons/compat_zen/script_component.hpp
+++ addons/compat_zen/script_component.hpp
@@ -11,3 +11,14 @@
 #endif
 
 #include "\x\iedd\addons\main\script_macros.hpp"
+
+#define IEDD_CLASSES [\
+	"iedd_ied_CanisterPlastic",\
+	"iedd_ied_CanisterFuel",\
+	"iedd_ied_Cardboard",\
+	"iedd_ied_Cinder",\
+	"iedd_ied_Metal",\
+	"iedd_ied_Metal_English",\
+	"iedd_ied_Barrel",\
+	"iedd_ied_Barrel_Grey"\
+]
