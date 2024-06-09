6b51a9dbcc6500b37de9b34a960f33b79a9cb073
Gabriel Nikol <GabrielNikol@web.de>
2024-06-09 19:15:41 +0200

    Add training IED Ammo.
    Training IED Ammo generates red smoke for 4 seconds and play annoying whistle sound.

--- addons/ied/CfgAmmo.hpp
+++ addons/ied/CfgAmmo.hpp
@@ -6,4 +6,23 @@ class CfgAmmo {
 		mass = 0;
 		displayName = CSTRING(Charge_Ammo_DisplayName);
 	};
+	class SmokeShellRed;
+	class GVAR(Training_IED_Ammo):SmokeShellRed {
+		explosionTime=0.00001;
+		timeToLive=4;
+		SmokeShellSoundLoop1[]=
+		{
+			"x\iedd\addons\ied\sounds\whistle",
+			100,
+			1,
+			15
+		};
+		grenadeBurningSound[]=
+		{
+			"SmokeShellSoundLoop1",
+			1.0
+		};
+		/*
+		*/
+	};
 };
