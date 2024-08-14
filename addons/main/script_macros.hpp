3d7a264436b47a4be32f300a807e871448aea45f
prisonerMO <prisoner2519@gmail.com>
2024-08-14 08:44:01 +0300

    removed authors define

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,7 +1,6 @@
 #include "\x\cba\addons\main\script_macros_common.hpp"
 #include "\x\cba\addons\xeh\script_xeh.hpp"
 #define AUTHOR "Prion"
-#define AUTHORS {"Prisoner","Lion"}
 
 #ifdef DISABLE_COMPILE_CACHE
     #undef PREP
