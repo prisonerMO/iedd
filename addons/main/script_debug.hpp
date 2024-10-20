4b174fa56e6b88c24657c24c7c02f0f96a84c468
prisonerMO <prisoner2519@gmail.com>
2024-10-20 19:14:16 +0300

    Disable compile cache

--- addons/main/script_debug.hpp
+++ addons/main/script_debug.hpp
@@ -1,4 +1,12 @@
-
+#ifdef DISABLE_COMPILE_CACHE
+    #define LINKFUNC(x) {_this call FUNC(x)}
+    #define PREP_RECOMPILE_START    if (isNil "IEDD_PREP_RECOMPILE") then {IEDD_RECOMPILES = []; IEDD_PREP_RECOMPILE = {{call _x} forEach IEDD_RECOMPILES;}}; private _recomp = {
+    #define PREP_RECOMPILE_END      }; call _recomp; IEDD_RECOMPILES pushBack _recomp;
+#else
+    #define LINKFUNC(x) FUNC(x)
+    #define PREP_RECOMPILE_START ; /* disabled */
+    #define PREP_RECOMPILE_END ; /* disabled */
+#endif
 /**
 Enable debug cba settings
 **/
