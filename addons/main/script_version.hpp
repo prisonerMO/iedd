b38a1c003faa42526f4d1fa0ac39ea4c429dfca4
prisonerMO <prisoner2519@gmail.com>
2025-01-10 08:02:41 +0200

    Version update 1.8.1

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,8 +1,8 @@
 #define MAJOR 1
 #define MINOR 8
-#define PATCHLVL 0
-#define BUILD 25
+#define PATCHLVL 1
+#define BUILD 26250110
 /******
-Build 26 to next release
+Build 27 to next release
 Current dev 1.8.1.26****
 *******/
