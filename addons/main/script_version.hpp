577a189241fe69a9e836072f7e1d93fe8e76ac01
prisonerMO <prisoner2519@gmail.com>
2025-04-21 17:50:28 +0300

    Version update

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,8 +1,8 @@
 #define MAJOR 1
 #define MINOR 9
 #define PATCHLVL 0
-#define BUILD 29250504
+#define BUILD 30250504
 /******
-Build 29 to next release
-Current dev 1.9.0.29****
+Build 30 to next release
+Current dev 1.9.0.30****
 *******/
