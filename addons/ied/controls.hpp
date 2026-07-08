c4ff91315230282ce008bd36e83f389590f6a016
prisonerMO <prisoner2519@gmail.com>
2026-07-08 17:17:08 +0300

    added check and expression if only isBury

--- addons/ied/controls.hpp
+++ addons/ied/controls.hpp
@@ -272,7 +272,7 @@ class GVAR(burySlider): Slider {
     private _slider = _ctrlGroup controlsGroupCtrl 100;\
     private _edit = _ctrlGroup controlsGroupCtrl 101;\
     _slider sliderSetPosition _value;\
-    _edit ctrlSetText ([_value, 0] call CBA_fnc_formatNumber) + 'step';";
+    _edit ctrlSetText (if (_value < 5) then {([5, 0] call CBA_fnc_formatNumber) + 'step'} else {([_value, 0] call CBA_fnc_formatNumber) + 'step'});";
     attributeSave = "params [""_ctrlGroup""];\
     sliderPosition (_ctrlGroup controlsGroupCtrl 100); ";
     onLoad = "params [""_ctrlGroup""];\
